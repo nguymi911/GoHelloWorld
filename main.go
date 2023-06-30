377203a0a7540cecc6d708e416fd82015cb17806
nguymi911 <134183728+nguymi911@users.noreply.github.com>
2023-06-30 10:21:14 +0000

    Added test route

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/test", HelloServer)
 	http.ListenAndServe(":8080", nil)
 }
 func HelloServer(w http.ResponseWriter, r *http.Request) {
